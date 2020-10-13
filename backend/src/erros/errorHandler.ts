2edad8fdc1024956a8b9a0b92cec061af2c295e3
Bruno <br.ponsoni@gmail.com>
2020-10-13 16:01:03 -0300

    Segundo dia: Criação do backend

---
+++ backend/src/erros/errorHandler.ts
@@ -0,0 +1,25 @@
+import { ErrorRequestHandler } from 'express'
+import { ValidationError } from 'yup'
+
+interface ValidationsErrors
+{
+    [key: string]: string[];
+}
+
+const errorHandler: ErrorRequestHandler = (error, request, response, next) => {
+    console.log(error);
+
+    if(error instanceof ValidationError){
+        let errors: ValidationsErrors = {};
+
+        error.inner.forEach(err => {
+            errors[err.path] = err.errors;
+        });
+
+        return response.status(400).json({message: 'Validation fails', errors})
+    }
+
+    return response.status(500).json({message: 'Internal Server Error'});
+}
+
+export default errorHandler;
